2db59dfde16af94596c8092e52f85793d0c35a2c
getwater-maker <getwater@gmail.com>
2025-08-25 00:16:29 +0900

    Update scene-parser.js

--- js/scene-parser.js
+++ js/scene-parser.js
@@ -292,7 +292,7 @@
     }
     if (rightTitle) {
       const blocks = parseSceneBlocks(promptInput ? promptInput.value : '');
-      rightTitle.textContent = `이미지 프롬프트 입력창 / 장면 갯수 ${blocks.length}`;
+      rightTitle.textContent = `이미지 프롬프트 입력창 / 장면 갯수 ${blocks.length} 개`;
     }
   }
 
@@ -504,3 +504,4 @@
     }
   }
 })();
+
